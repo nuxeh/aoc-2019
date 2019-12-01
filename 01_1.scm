cc4ffe071c7bc5a4aa16c6e778da0142d2920c4f
Ed Cragg <drq.11235@gmail.com>
2019-12-01 21:39:54 +0800

    Attempt to accumulate

--- 01_1.scm
+++ 01_1.scm
@@ -1,11 +1,14 @@
 #!/usr/bin/guile -s
 !#
 
+(let ((t 0))
 (do ((c (read) (read)))
     ((eof-object? c) 'done)
   (let ((x (- (floor (/ c 3)) 2)))
+  (+ t x)
   (display x))
   (newline))
+(display t))
 
 #!
 (let ((x (read)))
