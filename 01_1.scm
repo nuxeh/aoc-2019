19f4138ff9c38190b1d1ac6367e24c374039b66b
Ed Cragg <drq.11235@gmail.com>
2019-12-02 09:10:49 +0800

    Make proc

--- 01_1.scm
+++ 01_1.scm
@@ -1,17 +1,23 @@
 #!/usr/bin/guile -s
 !#
 
+(define fuel
+  (lambda (mass)
+    ((- (floor (/ mass 3)) 2))))
+
+(define (fuel2 m)
+  (display m)
+  (newline)
+  ((- (floor (/ m 3)) 2)))
+
+(define (foo n)
+  (foo (1+ n)))
+
 (let ((t 0))
 (do ((c (read) (read)))
     ((eof-object? c) 'done)
-  (let ((x (- (floor (/ c 3)) 2)))
+  (let ((x (fuel2 c)))
   (set! t (+ t x))
   (display x))
   (newline))
 (display t))
-
-#!
-(let ((x (read)))
-  (display (* x x))
-  (newline))
-!#
