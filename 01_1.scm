e6382710e4ae956a44cb0107d7d692a952f5b465
Ed Cragg <drq.11235@gmail.com>
2019-12-02 09:14:15 +0800

    Swap files around

--- 01_1.scm
+++ 01_1.scm
@@ -1,23 +1,17 @@
 #!/usr/bin/guile -s
 !#
 
-(define fuel
-  (lambda (mass)
-    ((- (floor (/ mass 3)) 2))))
-
-(define (fuel2 m)
-  (display m)
-  (newline)
-  ((- (floor (/ m 3)) 2)))
-
-(define (foo n)
-  (foo (1+ n)))
-
 (let ((t 0))
 (do ((c (read) (read)))
     ((eof-object? c) 'done)
-  (let ((x (fuel2 c)))
+  (let ((x (- (floor (/ c 3)) 2)))
   (set! t (+ t x))
   (display x))
   (newline))
 (display t))
+
+#!
+(let ((x (read)))
+  (display (* x x))
+  (newline))
+!#
