c9d09748a605df00e36ab2fc09ba7a2d8b6995e0
Ed Cragg <drq.11235@gmail.com>
2019-12-03 11:45:44 +0800

    Attempt at multiple return values

--- 02_1.scm
+++ 02_1.scm
@@ -1,13 +1,17 @@
+(use-modules (srfi srfi-1))
+(import (rnrs))
+
 (define (get-sublist l s f)
   (list-head (list-tail l 4) f))
 
 (define (recurse l p r) #t)
 
 (define (run l r)
-  (split-at l 4))
+  (let-values (((i s) (split-at l 4)))
+    (display (string-append i " -> " s))))
 
 (let ((x (read)))
   (let ((i (string-split (symbol->string x) #\,)))
     (display i)
     (newline)
-    (display (list-head i 4))))
+    (display (run i (list 0)))))
