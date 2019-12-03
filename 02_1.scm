12faaed648769454c81aa4478ec788478955c7d0
Ed Cragg <drq.11235@gmail.com>
2019-12-03 11:23:58 +0800

    Wip

--- 02_1.scm
+++ 02_1.scm
@@ -1,3 +1,11 @@
+(define (get-sublist l s f)
+  (list-head (list-tail l 4) f))
+
+(define (recurse l p r) #t)
+
+(define (run l r)
+  (split-at l 4))
+
 (let ((x (read)))
   (let ((i (string-split (symbol->string x) #\,)))
     (display i)
