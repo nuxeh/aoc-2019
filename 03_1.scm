b50da083ae15ca936fc339c79fc17e617ba38d88
Ed Cragg <drq.11235@gmail.com>
2019-12-04 18:03:44 +0800

    WIP
    (R8 U5 L5 D3)
    (U7 R6 D4 L4)
    ()()⏎

--- 03_1.scm
+++ 03_1.scm
@@ -1,3 +1,12 @@
+(define (get-path cmds p)
+  (if (null? (cdr cmds))
+    p
+    (get-path (cdr cmds) p)))
+
+(define (navigate cmd)
+  (if (eq (first cmd))
+      (display "up")))
+
 (let ((p1 (read)))
   (let ((p2 (read)))
     (let ((i (string-split (symbol->string p1) #\,)))
@@ -5,5 +14,11 @@
     (display i)
     (newline)
     (display j)
-    (newline)))))
+    (newline)
+    (let ((p/1 (get-path i (list))))
+    (let ((p/2 (get-path j (list))))
+      (display p/1)
+      (display p/2)
+    ))))))
+
 
