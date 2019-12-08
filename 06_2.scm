0a97c95976f0b4ecb129d0921a301e4e139f181f
Ed Cragg <drq.11235@gmail.com>
2019-12-09 00:58:20 +0800

    WIP

--- 06_2.scm
+++ 06_2.scm
@@ -1,15 +1,18 @@
 (use-modules (srfi srfi-1))
 
+;recurse backwards from node
 (define (build-tree orbs name d)
   (let ((cs (filter (lambda (o) (equal? (second o) name)) orbs)))
     (display name)(newline)
+    (display cs)(newline)
+    (display d)(newline)
     (if (eq? (length cs) 0)
         d
         (map (lambda (c) (build-tree orbs (first c) (append d (list name)))) cs))))
 
 (define (get-inter-santa-dist orbs)
-  (build-tree orbs "SAN" '())
-  (build-tree orbs "YOU" '()))
+  (display (build-tree orbs "SAN" '()))
+  (map display (build-tree orbs "YOU" '())))
 
 (define (get-orbits)
   (let ((o '()))
@@ -20,5 +23,5 @@
 
 (let ((orbits (get-orbits)))
   (display orbits)(newline)
-  (display (get-inter-santa-dist orbits))) ;part 2
+  (get-inter-santa-dist orbits)) ;part 2
 
