c924d9090418b9e53c934351bbd2fc891cb533c4
Ed Cragg <drq.11235@gmail.com>
2019-12-09 02:03:36 +0800

    Parse image dimensions and read data

---
+++ 08_1.scm
@@ -0,0 +1,7 @@
+(define w (read))
+(define h (read))
+(display (string-append "w: " (number->string w) "\n"))
+(display (string-append "h: " (number->string h) "\n"))
+
+(let ((idata (read )))
+  (display idata))
