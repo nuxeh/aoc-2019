eefaee26b6aaa0272d20df5b34321efd1dc4e495
Ed Cragg <drq.11235@gmail.com>
2019-12-09 00:26:32 +0800

    Get a path back to COM
    ((COM B) (B C) (C D) (D E) (E F) (B G) (G H) (D I) (E J) (J K) (K L) (K YOU) (I SAN))
    SAN
    I
    D
    C
    B
    COM
    YOU
    K
    J
    E
    D
    C
    B
    COM
    ((((((((YOU K J E D C B))))))))⏎

--- 06_2.scm
+++ 06_2.scm
@@ -3,17 +3,13 @@
 (define (build-tree orbs name d)
   (let ((cs (filter (lambda (o) (equal? (second o) name)) orbs)))
     (display name)(newline)
-    (if (equal? name "SAN")
-        (display (string-append "SAN: " (number->string d) "\n")) )
-    (if (equal? name "YOU")
-        (display (string-append "YOU: " (number->string d) "\n")) )
     (if (eq? (length cs) 0)
         d
-        (fold + d (map (lambda (c) (build-tree orbs (first c) (1+ d))) cs)))))
+        (map (lambda (c) (build-tree orbs (first c) (append d (list name)))) cs))))
 
 (define (get-inter-santa-dist orbs)
-  (build-tree orbs "SAN" 0)
-  (build-tree orbs "YOU" 0))
+  (build-tree orbs "SAN" '())
+  (build-tree orbs "YOU" '()))
 
 (define (get-orbits)
   (let ((o '()))
