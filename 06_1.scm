b449723822a9a1476d4e1521489ea0933fe83744
Ed Cragg <drq.11235@gmail.com>
2019-12-08 22:55:36 +0800

    Accumulate sums up the tree

--- 06_1.scm
+++ 06_1.scm
@@ -4,10 +4,11 @@
   (let ((cs (filter (lambda (o) (equal? (car o) name)) orbs)))
     (display cs)
     (for-each (lambda (o) (display (second o))(display " ")) cs)
+    (display " ")(display d)
     (newline)
     (if (eq? (length cs) 0)
         d
-        (fold + 0 (map (lambda (c) (build-tree orbs (second c) (1+ d))) cs)))))
+        (fold + d (map (lambda (c) (build-tree orbs (second c) (1+ d))) cs)))))
 
 (define (get-orbits)
   (let ((o '()))
