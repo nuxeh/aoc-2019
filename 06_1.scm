9b7661a1c9ea99214d2b0e8d6f29ec13f5f9d5d5
Ed Cragg <drq.11235@gmail.com>
2019-12-08 22:51:35 +0800

    Shorten variable names

--- 06_1.scm
+++ 06_1.scm
@@ -1,13 +1,13 @@
 (use-modules (srfi srfi-1))
 
-(define (build-tree orbits name depth)
-  (let ((children (filter (lambda (o) (equal? (car o) name)) orbits)))
-    (display children)
-    (for-each (lambda (o) (display (second o))(display " ")) children)
+(define (build-tree orbs name d)
+  (let ((cs (filter (lambda (o) (equal? (car o) name)) orbs)))
+    (display cs)
+    (for-each (lambda (o) (display (second o))(display " ")) cs)
     (newline)
-    (if (eq? (length children) 0)
-        depth
-        (fold + 0 (map (lambda (c) (build-tree orbits (second c) (1+ depth))) children)))))
+    (if (eq? (length cs) 0)
+        d
+        (fold + 0 (map (lambda (c) (build-tree orbs (second c) (1+ d))) cs)))))
 
 (define (get-orbits)
   (let ((o '()))
