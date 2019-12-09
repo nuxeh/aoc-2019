f25d1ebf3b6df31e752627b6b4d73ecbabe4a147
Ed Cragg <drq.11235@gmail.com>
2019-12-09 10:01:43 +0800

    WIP

--- 08_1.scm
+++ 08_1.scm
@@ -1,7 +1,13 @@
+(use-modules (srfi srfi-1))
+
 (define w (read))
 (define h (read))
 (display (string-append "w: " (number->string w) "\n"))
 (display (string-append "h: " (number->string h) "\n"))
+
+(define (image-chunk imgdata w h)
+  (let-values ((head tail (split-at imgdata w)))
+
 
 (let ((idata (read )))
   (display idata))
