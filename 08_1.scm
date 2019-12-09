9eefb0f299b5f8f4e91e8c54574f376a73ebdb3d
Ed Cragg <drq.11235@gmail.com>
2019-12-09 10:34:51 +0800

    Count zeroes in each layer
    w: 3
    h: 2
    (1 2 3 4 5 6)0(7 8 9 0 1 2)1⏎

--- 08_1.scm
+++ 08_1.scm
@@ -6,13 +6,22 @@
 (display (string-append "w: " (number->string w) "\n"))
 (display (string-append "h: " (number->string h) "\n"))
 
+(define (count-zeroes l acc)
+  (if (eq? l 0)
+      (+ acc 1)
+      acc))
+
 (define (image-chunk imgdata w h)
   (let-values (((head tail) (split-at imgdata (* w h))))
     (display head)
+    (let ((nz (fold count-zeroes 0 head)))
+      (display nz))
     (if (null? tail)
         #t
         (image-chunk tail w h))))
 
+(define (char->number a)
+  (string->number (string a)))
 
-(let ((idata (string->list (number->string (read )))))
+(let ((idata (map char->number (string->list (number->string (read ))))))
   (image-chunk idata w h))
