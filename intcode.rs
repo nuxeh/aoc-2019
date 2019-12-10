552c61be6045fc85651fc550f5fcf050c19bb103
Ed Cragg <drq.11235@gmail.com>
2019-12-10 12:03:33 +0800

    Return array rather than tuple

--- intcode.rs
+++ intcode.rs
@@ -13,6 +13,7 @@ struct OutputParam {
     value: i16,
 }
 
+#[allow(non_camel_case_types)]
 enum Intcode {
     ADD(InputParam, InputParam, OutputParam),
     MULT(InputParam, InputParam, OutputParam),
@@ -20,12 +21,12 @@ enum Intcode {
     DISP(InputParam),
 }
 
-fn get_modes(intcode: i16) -> (i16, i16, i16, i16) {
+fn get_modes(mut intcode: i16) -> [i16; 4] {
   let c = intcode / 10000;
   intcode -= c * 10000;
   let b = intcode / 1000;
   intcode -= b * 1000;
   let a = intcode / 100;
   intcode -= a * 100;
-  (intcode, c, b, a)
+  [intcode, c, b, a]
 }
