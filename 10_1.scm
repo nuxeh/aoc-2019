2312b20917561eefe5a7d4a805b10d90e1600722
Ed Cragg <drq.11235@gmail.com>
2019-12-10 14:52:11 +0800

    Convert to bitvector
        #*0100100000111110000100011⏎

--- 10_1.scm
+++ 10_1.scm
@@ -1,7 +1,14 @@
 (use-modules (ice-9 rdelim))
 
+(define (to-bit v)
+  (if (equal? v #\#)
+      #t
+      #f))
+
 (let ((s ""))
   (do ((c (read-line) (read-line)))
       ((eof-object? c) 'done)
-      (set! s (string-append s c))
-      (display (string->list s))(newline)))
+      (set! s (string-append s c)))
+  (let ((l (string->list s)))
+    (let ((m (list->bitvector (map to-bit l))))
+      (display m))))
