9a89142c3fdc146b357b57f6feb2559656caf558
Ed Cragg <drq.11235@gmail.com>
2019-12-10 12:07:49 +0800

    Add len() fn for Intcode

--- intcode.rs
+++ intcode.rs
@@ -21,6 +21,17 @@ enum Intcode {
     DISP(InputParam),
 }
 
+impl Intcode {
+    fn len(&self) -> usize {
+        match self {
+            Self::ADD(_, _, _) => 4,
+            Self::MULT(_, _, _) => 4,
+            Self::SET(_) => 2,
+            Self::DISP(_) => 2,
+        }
+    }
+}
+
 fn get_modes(mut intcode: i16) -> [i16; 4] {
   let c = intcode / 10000;
   intcode -= c * 10000;
