3e10e3bbf173dc480eb37e6ec953b60dc6abea50
Ed Cragg <drq.11235@gmail.com>
2019-12-19 23:22:44 +0800

    WIP

--- 04_1.scm
+++ 04_1.scm
@@ -4,13 +4,22 @@
 (define (recurse n found)
   (define z (splitz n))
   (if (<= n end)
-      (if (valid z)
+      (if (valid? z)
 	  (recurse (1+ n) (append found (list n)))
 	  (recurse (1+ n) found))
       found))
 
-(define (valid z)
-  #t)
+(define (valid? z)
+  (and (ascends? z #f) (contains-double? z #f)))
+
+(define (contains-double? z r)
+
+
+
+(define (ascends? z)
+  (fold-right (lambda (a b) (if (< a b)
+				#f
+
 
 (define (splitz n)
   (map char->integer (string->list (number->string n))))
