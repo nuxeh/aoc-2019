dde3e1ac4194fe01716b55e4064501827535ba98
Ed Cragg <drq.11235@gmail.com>
2019-12-19 21:14:18 +0800

    Attempt to chain amplifiers with slightly strange results

--- 07_1.rs
+++ 07_1.rs
@@ -14,20 +14,36 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{}", amp(&mem, 20000, 0));
     let mut count = 0;
+    let mut best = 0;
+    let mut best_phases = Vec::new();
     for a in 0..=4 {
         for b in 0..=4 {
             for c in 0..=4 {
                 for d in 0..=4 {
                     for e in 0..=4 {
                         count += 1;
+                        let out = chain(&mem, a, b, c, d, e);
+                        if out > best {
+                            best = out;
+                            best_phases = vec!(a, b, c, d, e);
+                        }
                     }
                 }
             }
         }
     }
     println!("{}", count);
+    println!("{}", best);
+    println!("{:?}", best_phases);
+}
+
+fn chain(mem: &Vec<i64>, a: i64, b: i64, c: i64, d: i64, e: i64) -> i64 {
+    let l = amp(mem, 0, a);
+    let m = amp(mem, l, b);
+    let n = amp(mem, m, c);
+    let o = amp(mem, n, d);
+    amp(mem, o, e)
 }
 
 fn amp(mem: &Vec<i64>, input: i64, phase: i64) -> i64 {
