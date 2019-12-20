db87152ac05d17e063bf0fc426eca123f3c23e55
Ed Cragg <drq.11235@gmail.com>
2019-12-20 22:59:15 +0800

    Print
    ./19_1 19_1_i1.ic 25 2> /dev/null | guile 19_2.scm
    (25 25)
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................
    .........................

--- 19_2.scm
+++ 19_2.scm
@@ -8,41 +8,63 @@
 (define (process-line y array l)
   (define x 0)
   (let ((d (map char->bit (string->list l))))
-    (display d)(newline)
     (map (lambda (e) (if e
 			 (array-set! array #t x y))
 	   (set! x (1+ x))) d)))
 
 (define lines 0)
 (define array #f)
+(define dim #f)
 
 ;read input
 (do ((c (read-line) (read-line)))
   ((eof-object? c) 'done)
-  (let ((dim (string-length c)))
+  (let ((wid (string-length c)))
+    (if (not dim)
+	(set! dim wid))
     (if (not array)
-	(set! array (make-array #f dim dim)))
+	(set! array (make-array #f wid wid)))
     (process-line lines array c)
     (set! lines (1+ lines))))
-
-(display array)(newline)
 
 (define (scan-y i j d)
   (if (< d 10)
       (if (array-ref array i j)
-	  (scan-y i (1+ j) (1+ d))
+	  (if (< j (1- dim))
+	      (scan-y i (1+ j) (1+ d))
+	      #f)
 	  #f)
       #t))
 
 (define (scan-x i j d)
   (if (< d 10)
       (if (scan-y i j 0)
-	  (scan-x (+1 i) j (1+ d))
+	  (if (< i (1- dim))
+	      (scan-x (+1 i) j (1+ d))
+	      #f)
 	  #f)
       #t))
 
 (define (find-10x10 i j)
   (scan-x i j 0))
 
+(define (print-line array i j)
+  (if (< i dim)
+      (begin
+	(if (array-ref array i j)
+	    (display #\#)
+	    (display #\.))
+	(print-line array (1+ i) j))))
+
+(define (array-print array j)
+  (if (< j dim)
+      (begin
+	(print-line array 0 j)
+	(newline)
+	(array-print array (1+ j)))))
+
 (display (array-index-map! array find-10x10))(newline)
-(display array)(newline)
+(display (array-dimensions array))(newline)
+(array-print array 0)
+
+
