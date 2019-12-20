b3f3c7f228f408f1f5a8abbd0ff10d5dddc30350
Ed Cragg <drq.11235@gmail.com>
2019-12-20 21:03:56 +0800

    char->bit
    ./19_1 19_1_i1.ic 20 2> /dev/null | guile 19_2.scm
    (#t #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #t #t #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #t #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #t #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #t)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
    (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)

--- 19_2.scm
+++ 19_2.scm
@@ -1,6 +1,11 @@
 (use-modules (ice-9 rdelim)) ;read-line
 
+(define (char->bit x)
+  (if (equal? x #\#)
+      #t
+      #f))
+
 (do ((c (read-line) (read-line)))
   ((eof-object? c) 'done)
-  (let ((d (string->list c)))
+  (let ((d (map char->bit (string->list c))))
     (display d)(newline)))
