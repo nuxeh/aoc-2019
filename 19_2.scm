12df10aa73ee07f558bd45868c04f60e02857b56
Ed Cragg <drq.11235@gmail.com>
2019-12-20 21:31:27 +0800

    Make into a 2d array
        /19_1 19_1_i1.ic 20 2> /dev/null | guile 19_2.scm
        (#t #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        #...................
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        ....................
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        ....................
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        ....................
        (#f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        .....#..............
        (#f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f #f)
        ......#.............
        (#f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f)
        .......#............
        (#f #f #f #f #f #f #f #f #t #t #f #f #f #f #f #f #f #f #f #f)
        ........##..........
        (#f #f #f #f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f)
        ..........#.........
        (#f #f #f #f #f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f)
        ...........#........
        (#f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f #f)
        ............##......
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f)
        .............##.....
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f)
        ..............##....
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #t #f #f)
        ...............###..
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #t #f)
        ................###.
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #t)
        .................###
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t)
        ...................#
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        ....................
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        ....................
        (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)
        ....................
        #2((#t #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #t #f #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #t #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f #f) (#f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #t #t #f #f #f))

--- 19_2.scm
+++ 19_2.scm
@@ -2,15 +2,27 @@
 
 (define (char->bit x)
   (if (equal? x #\#)
-      1
-      0))
+      #t
+      #f))
 
-(define (process-line l)
-  (let ((dim (string-length l)))
-    (let ((d (map char->bit (string->list l))))
-      d)))
+(define (process-line y array l)
+  (define x 0)
+  (let ((d (map char->bit (string->list l))))
+    (display d)(newline)
+    (map (lambda (e) (if e
+			 (array-set! array #t x y))
+	   (set! x (1+ x))) d)))
+
+(define lines 0)
+(define array #f)
 
 (do ((c (read-line) (read-line)))
   ((eof-object? c) 'done)
-  (let ((d (process-line c)))
-    (display d)(newline)))
+  (let ((dim (string-length c)))
+    (if (not array)
+	(set! array (make-array #f dim dim)))
+    (process-line lines array c)
+    (display c)(newline)
+    (set! lines (1+ lines))))
+
+(display array)(newline)
