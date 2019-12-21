06b5049e49bc198904a714f4add6f29f54b672c2
Ed Cragg <drq.11235@gmail.com>
2019-12-21 20:03:57 +0800

    WIP try to find intersections

--- 17_1.scm
+++ 17_1.scm
@@ -9,3 +9,27 @@
 
 (display input)(newline)
 
+(define (is-intersection? line)#t)
+
+;(define (x-intersect lline)
+;  (if (not (null? (cadr lline)))
+;      (if (and (equal? (car lline) #\#) (equal? (cadr lline) #\#) (equal? (caddr lline) #\#))
+;	  (set! (cadr lline) #\#)
+
+(define (y-intersect elems)
+  (if (and (eq? (car elems) #\#) (eq? (cadr elems) #\#) (eq? (caddr) #\#))
+      (list (car elems) #\* (caddr elems))
+      elems))
+
+(define (process-line lines)
+  (if (not (null? (caddr lines)))
+      (begin
+	(map y-intersect (car lines) (cadr lines) (caddr lines))
+	(process-line (cadddr lines)))))
+
+(define (display-line line)
+  (for-each (lambda (c) (display c)) line)
+  (newline))
+
+(process-line input)
+(for-each display-line input)
