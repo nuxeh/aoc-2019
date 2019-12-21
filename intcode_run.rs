51bc3299de8492d217c48d479fc4609ca3d50a28
Ed Cragg <drq.11235@gmail.com>
2019-12-21 13:20:22 +0800

    Optionally print ascii
        ./intcode_run 17_1_i1.ic 1 --ascii
        ......#########..............................
        ......#.......#..............................
        ......#.......#.......................######^
        ......#.......#.......................#......
        ......#.......#.......................#......
        ......#.......#.......................#......
        ......#####...#...........#############......
        ..........#...#...........#..................
        ......###########.........#..................
        ......#...#...#.#.........#..................
        ......#...#...#.#.........#..................
        ......#...#...#.#.........#..................
        ......#...#...#############..................
        ......#...#.....#............................
        ......#######...#............................
        ..........#.#...#............................
        ..........#.#...#............................
        ..........#.#...#............................
        ..........#######............................
        ............#................................
        ............#................................
        ............#................................
        ............#...........#######..............
        ............#...........#.....#..............
        ........###########.....#.....#..............
        ........#...#.....#.....#.....#..............
        #############.....#.....#.....#...#..........
        #.......#.........#.....#.....#...#..........
        #.......#.....#############...#...#..........
        #.......#.....#...#.....#.#...#...#..........
        #.....#############.....#.#...#...#..........
        #.....#.#.....#.........#.#...#...#..........
        #.....#.#.....#.........###########..........
        #.....#.#.....#...........#...#..............
        #######.#######...........#...#####..........
        ..........................#.......#..........
        ..........................#.......#..........
        ..........................#.......#..........
        ..........................#.......#..........
        ..........................#.......#..........
        ..........................#########..........

--- intcode_run.rs
+++ intcode_run.rs
@@ -19,8 +19,6 @@ fn main() {
     let in_path: PathBuf = env::args().into_iter().skip(1).take(1).collect();
     let input: String = env::args().into_iter().skip(2).take(1).collect();
 
-    println!("{:?}", flag("--ascii"));
-
     let mem = intcode::read_file(&in_path).unwrap_or_else(|e| {
         eprintln!("error: {}", e);
         process::exit(1);
@@ -36,5 +34,9 @@ fn main() {
         eprintln!("error in execution: {}", e);
     });
 
-    println!("{:?}", comp.outputs);
+    if flag("--ascii") {
+        comp.outputs.iter().for_each(|o| print!("{}", (*o as u8) as char));
+    } else {
+        println!("{:?}", comp.outputs);
+    };
 }
