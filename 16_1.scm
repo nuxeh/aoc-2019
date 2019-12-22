56e17f925c7d1e08016c584cdd0eff396ea54f1c
Ed Cragg <drq.11235@gmail.com>
2019-12-22 16:24:22 +0800

    Run phase gen for each input digit
    echo 12345678 | guile 16_1.scm
    (1 2 3 4 5 6 7 8)
    8
    (1 2 3 4 5 6 7 8)

--- 16_1.scm
+++ 16_1.scm
@@ -1,12 +1,34 @@
 (use-modules (ice-9 rdelim)) ;read-line
+(use-modules (srfi srfi-1))  ;differing length map
 
 (define in (map (lambda (v) (- v 48)) (map char->integer (string->list (read-line)))))
+(define l (length in))
 (display in)(newline)
-(display (length in))
+(display l)(newline)
 
+(define cycle '(0 1 0 -1))
 
+#!
+0  1  0 -1  0  1  0 -1  0  1  0 -1
+
+0  0  1  1  0  0 -1 -1  0  0  1  1
+
+0  0  0  1  1  1  0  0  0 -1 -1 -1
+
+0  0  0  0  1  1  1  1  0  0  0  0
+!#
+
+(define (gen-phase n) n)
+  
+
+(define (gen-phases input)
+  (define p 0)
+  (map (lambda (_) (set! p (1+ p))(gen-phase p)) input))
+
+(define phases (gen-phases in))
+(display phases)(newline)
 
 (define (gen-phase p len cp cv res)
   (if (<= cp p)
       (gen-phase (1+ p) nv res)
-      (cdr res))
+      (cdr res)))
