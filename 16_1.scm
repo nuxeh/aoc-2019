cc90b168552ce41622cc650c8b6500f348a5efd8
Ed Cragg <drq.11235@gmail.com>
2019-12-22 16:03:47 +0800

    WIP

--- 16_1.scm
+++ 16_1.scm
@@ -1,4 +1,12 @@
 (use-modules (ice-9 rdelim)) ;read-line
 
 (define in (map (lambda (v) (- v 48)) (map char->integer (string->list (read-line)))))
-(display in)
+(display in)(newline)
+(display (length in))
+
+
+
+(define (gen-phase p len cp cv res)
+  (if (<= cp p)
+      (gen-phase (1+ p) nv res)
+      (cdr res))
