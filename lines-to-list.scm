5a06b47a6f10ad5e332a6e3987ba9fd2030fafe3
Ed Cragg <drq.11235@gmail.com>
2019-12-24 19:54:24 +0800

    Add lines to list script

---
+++ lines-to-list.scm
@@ -0,0 +1,10 @@
+#!/usr/bin/guile -s
+!#
+
+(use-modules (ice-9 rdelim)) ;read-line
+
+(define result '())
+(do ((c (read-line) (read-line)))
+  ((eof-object? c) 'done)
+  (set! result  (append result (list (string->list c)))))
+(display result)(newline)
