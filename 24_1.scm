51fce75191af6d33110857aa84cf14709d298dc1
Ed Cragg <drq.11235@gmail.com>
2019-12-24 20:37:00 +0800

    List to array

--- 24_1.scm
+++ 24_1.scm
@@ -1,10 +1,22 @@
 (use-modules (ice-9 pretty-print))
 
 (define input (read))
-(define array (make-array #\. (length input) (length input)))
+(define arr (make-array #\. (length input) (length input)))
 
 (define (print-grid g)
   (pretty-print g #:display? #t))
 
+(define x 0)
+(define y 0)
+(for-each (lambda (line)
+	    (for-each (lambda (char)
+			(display x)(display ", ")(display y)(newline)
+			(array-set! arr char y x)
+			(set! x (1+ x)))
+		      line)
+	    (set! y (1+ y))
+	    (set! x 0))
+	  input)
+
 (display input)(newline)
-(print-grid array)
+(print-grid arr)
