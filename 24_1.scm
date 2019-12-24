9ac7833176dd3d5e42a40486d875088915abfaea
Ed Cragg <drq.11235@gmail.com>
2019-12-24 20:27:10 +0800

    Pretty print input and make array

--- 24_1.scm
+++ 24_1.scm
@@ -1,2 +1,10 @@
+(use-modules (ice-9 pretty-print))
+
 (define input (read))
+(define array (make-array #\. (length input) (length input)))
+
+(define (print-grid g)
+  (pretty-print g #:display? #t))
+
 (display input)(newline)
+(print-grid array)
