7a9bae5ddc85185de890c409120955b3c3605201
Ed Cragg <drq.11235@gmail.com>
2019-12-26 15:12:51 +0800

    Give intcode run a bit of a REPL for inputs

--- intcode_run.rs
+++ intcode_run.rs
@@ -1,10 +1,11 @@
 use std::env;
-use std::process;
+use std::io::{stdin, stdout, Write};
 use std::path::PathBuf;
+use std::process;
 
 mod intcode;
 
-use intcode::IntcodeComputer;
+use intcode::{IntcodeComputer, Status};
 
 fn flag(flag: &str) -> bool {
     for arg in env::args() {
@@ -17,7 +18,6 @@ fn flag(flag: &str) -> bool {
 
 fn main() {
     let in_path: PathBuf = env::args().into_iter().skip(1).take(1).collect();
-    let input: String = env::args().into_iter().skip(2).take(1).collect();
 
     let mem = intcode::read_file(&in_path).unwrap_or_else(|e| {
         eprintln!("error: {}", e);
@@ -26,17 +26,39 @@ fn main() {
 
     let mut comp = IntcodeComputer::new()
         .load_mem_pad(&mem)
-        .input(input.parse().unwrap())
         .debug(false)
         .init();
 
-    comp.run().unwrap_or_else(|e| {
-        eprintln!("error in execution: {}", e);
-    });
+    loop {
+        if comp.status == Status::WaitingForInput {
+            comp.give_input(get_input());
+        }
 
-    if flag("--ascii") {
-        comp.outputs.iter().for_each(|o| print!("{}", (*o as u8) as char));
-    } else {
-        println!("{:?}", comp.outputs);
-    };
+        comp.run().unwrap_or_else(|e| {
+            eprintln!("error in execution: {}", e);
+        });
+
+        if flag("--ascii") {
+            comp.outputs
+                .drain(..)
+                .for_each(|o| print!("{}", (o as u8) as char));
+        } else {
+            let out: Vec<_> = comp.outputs.drain(..).collect();
+            println!("{:?}", out);
+        };
+    }
+}
+
+fn get_input() -> i64 {
+    loop {
+        let mut s = String::new();
+        print!("input> ");
+        let _ = stdout().flush();
+        if let Err(e) = stdin().read_line(&mut s) {
+            continue;
+        }
+        if let Ok(i) = s.trim().parse::<i64>() {
+            return i;
+        }
+    }
 }
